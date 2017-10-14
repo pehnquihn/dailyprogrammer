885e3a9adf26185e859d22c4efb6a701e6b6ffbf
Matthew Wahner <mattwahner@gmail.com>
2017-10-14 14:48:25 -0500

    Added solution to 335E

---
+++ solutions/335E/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    # Ensure correct amount of arguments were set
+    if len(sys.argv) != 3:
+        print('Not the correct amount of arguments!');
+        sys.exit(1)
+
+    # Read input file
+    with open(sys.argv[1], 'r') as f:
+        lines = f.read().split('\n')
+
+    # Parse gap
+    gap = int(sys.argv[2])
+
+    # Loop through input lines
+    for line in lines[1:-1]:
+        d = {int(value): distance for distance, value in enumerate(line.split())}
+        print(sum(map(lambda x: abs(d[x + 1] - d[x]), filter(lambda x: (x + 1) in d, d))))
+
+if __name__ == '__main__':
+    main()
+
